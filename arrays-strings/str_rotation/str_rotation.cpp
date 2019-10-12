a3f79ac5b81b64e56851c90c17ccc84253f1c9c5
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-10-12 22:15:56 +0100

    Solve 1.9 - String Rotation

--- arrays-strings/str_rotation/str_rotation.cpp
+++ arrays-strings/str_rotation/str_rotation.cpp
@@ -3,12 +3,34 @@
 
 using namespace std;
 
-int main() {
-  string big ("a long text");
-  string small ("long ");
-  if (big.find(small) != string::npos) {
-    cout << "yes";
+bool is_str_rot(string a, string b) {
+  a += a;
+  if (a.find(b) != string::npos) {
+    return true;
   } else {
-    cout << "no";
+    return false;
   }
+}
+
+void test(string a, string b, bool expected) {
+  try {
+    if (is_str_rot(a, b) != expected) {
+      throw a;
+    }
+  } catch (string e) {
+    cout << "is_str_rot() fails on \"" << e << "\"\n";
+  }
+}
+
+int main() {
+  string foo = "a long text";
+  string bar = "long texta ";
+
+  test(foo, bar, true);
+
+  foo = "b long text";
+
+  test(foo, bar, false);
+
+  cout << "success!\n";
 }
