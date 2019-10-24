4296ba343db29d1b619e4c0f0456b6be836f942d
Orfeas Stefanos Thyfronitis Litos <o.thyfronitis@ed.ac.uk>
2019-10-24 14:09:42 +0100

    Count 0's as well

--- hard/count_twos.js
+++ hard/count_twos.js
@@ -41,20 +41,26 @@ function split_number(n, rank) {
 }
 
 function count_digits_range_fast(n, wanted) {
-  if (n === 0 && wanted === 0) {
-    return 1
-  }
-
-  let count = 0
+  // count initial 0 when counting 0's
+  let count = (wanted === 0) ? 1 : 0
 
   for (let i = 0; 10**i <= n; i++) {
     let [smaller, digit, larger] = split_number(n, i)
 
-    count += larger * 10**i
+    // count appearances because of larger digits
+    if (wanted === 0) {
+      // do not count 0's that would appear at the top rank
+      count += Math.max(larger - 1, 0) * 10**i
+    } else {
+      count += larger * 10**i
+    }
+
+    // count appearances because of smaller digits
     if (digit === wanted) {
       count += smaller + 1
     }
-    else if (wanted < digit) {
+    else if (!(wanted === 0 && larger === 0) && wanted < digit) {
+      // do not count 0's that would appear at the top rank
       count += 10**i
     }
   }
@@ -62,9 +68,8 @@ function count_digits_range_fast(n, wanted) {
   return count
 }
 
-for (let i = 0; i < 400; i++) {
-  // TODO: find how to count 0's
-  for (let j = 1; j <= 9; j++) {
+for (let i = 0; i < 4000; i++) {
+  for (let j = 0; j <= 9; j++) {
     const naive = count_digits_range_naive(i, j)
     const fast = count_digits_range_fast(i, j)
     if (naive !== fast) {
