1b6018b86f687397858ee1c6263505579b0a1448
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-11-13 09:41:49 +0000

    Remove empty function from mwe

--- hard/circus_tower/mwe_weird_exit.cpp
+++ hard/circus_tower/mwe_weird_exit.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <map>
 
 class A {
   public:
@@ -10,19 +9,16 @@ class A {
 
 class B {
   public:
-    std::map<int, int> a_map;
-
-    B(std::vector<A *> vec) {
-      std::cout << vec[0]->x << std::endl;
+    B(std::vector<A *> pt_vec) {
+      std::cout << pt_vec[0]->x << std::endl;
     };
 };
-
-void func(B b) {}
 
 int main() {
   A a;
   a.x = 4;
-  std::vector<A> vec {a};
+  std::vector<A> vec;
+  vec.push_back(a);
   std::vector<A *> pt_vec;
   for (A el: vec) {
     pt_vec.push_back(&el);
@@ -33,5 +29,4 @@ int main() {
   // TODO: understand why if next line is around,
   //       previous line works as expected
   //exit(1);
-  func(b);
 }
