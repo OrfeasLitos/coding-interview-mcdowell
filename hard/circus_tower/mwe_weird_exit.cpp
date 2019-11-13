f73e1ca45b9a8c7fe6d3c1e695d6913c5ff54ab7
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-11-13 09:31:42 +0000

    Minimize weird behavior further

--- hard/circus_tower/mwe_weird_exit.cpp
+++ hard/circus_tower/mwe_weird_exit.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <map>
 
 class A {
   public:
     int x;
-    int y;
 };
 
 int main() {
   A a;
-  std::vector<A> vec {a};
+  std::vector<A> vec;
+  vec.push_back(a);
   a.x = 4;
-  std::vector<A *> pt_vec;
-  std::cout << vec[0].x << " " << a.x << std::endl;
+  std::cout << vec[0].x << std::endl;
   // TODO: understand why if next line is around, line 16 works as expected
   //exit(1);
 }
