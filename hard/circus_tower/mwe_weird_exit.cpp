a50a32aeb1cf0d78ff5c4d84dbddbeb5baceecce
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-11-13 09:27:05 +0000

    Minimize weird function further

--- hard/circus_tower/mwe_weird_exit.cpp
+++ hard/circus_tower/mwe_weird_exit.cpp
@@ -4,29 +4,16 @@
 
 class A {
   public:
-    int x = 4;
+    int x;
     int y;
 };
 
-class B {
-  public:
-    std::map<int, int> a_map;
-
-    B(std::vector<A *> vec) {
-      std::cout << vec[0]->x << '\n';
-    };
-};
-
-void func(B b) {}
-
 int main() {
-  std::vector<A> vec {A()};
-  std::vector<A *> pt_vec {};
-  for (A el: vec) {
-    pt_vec.push_back(&el);
-  }
-  auto b = B(pt_vec);
+  A a;
+  std::vector<A> vec {a};
+  a.x = 4;
+  std::vector<A *> pt_vec;
+  std::cout << vec[0].x << " " << a.x << std::endl;
   // TODO: understand why if next line is around, line 16 works as expected
-  exit(1);
-  func(b);
+  //exit(1);
 }
