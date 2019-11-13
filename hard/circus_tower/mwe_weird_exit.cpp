2b4fdd4696554596346cccfa960e9cae2df982cf
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2019-11-13 09:35:02 +0000

    Revert to previous, seemingly correct version

--- hard/circus_tower/mwe_weird_exit.cpp
+++ hard/circus_tower/mwe_weird_exit.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 class A {
   public:
     int x;
+    int y;
 };
+
+class B {
+  public:
+    std::map<int, int> a_map;
+
+    B(std::vector<A *> vec) {
+      std::cout << vec[0]->x << std::endl;
+    };
+};
+
+void func(B b) {}
 
 int main() {
   A a;
-  std::vector<A> vec;
-  vec.push_back(a);
   a.x = 4;
+  std::vector<A> vec {a};
+  std::vector<A *> pt_vec;
+  for (A el: vec) {
+    pt_vec.push_back(&el);
+  }
   std::cout << vec[0].x << std::endl;
-  // TODO: understand why if next line is around, line 16 works as expected
+  B b = B(pt_vec);
+  std::cout << vec[0].x << std::endl;
+  // TODO: understand why if next line is around,
+  //       previous line works as expected
   //exit(1);
+  func(b);
 }
